ff76d2281cff7db3c4472310a61c88f92dfa7d91
Wu, Zhenyu <wuzhenyu@ustc.edu>
2023-12-25 14:46:04 +0800

    :white_check_mark: Fix pytest

---
+++ tests/test_utils.py
@@ -0,0 +1,10 @@
+r"""Test utils."""
+from mutt_language_server.utils import get_schema
+
+
+class Test:
+    r"""Test."""
+
+    @staticmethod
+    def test_get_schema() -> None:
+        assert get_schema()["properties"].get("set")
